c6ce047de83df7baacc39910602618231b7809dd
Roman_Egorov <egorovrp1991@mail.ru>
2023-07-08 00:12:24 +0300

    End Program3

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -2,3 +2,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if( a % 2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else{
+    Console.WriteLine("Число не является чётным");
+}
+
